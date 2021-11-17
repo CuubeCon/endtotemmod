cf74e83eb737047b78e0f8bc9ff12624665aa1b5
Manuel <cubeconlp@gmail.com>
2021-11-17 19:57:21 +0100

    some fixes

--- src/main/java/io/github/cuubecon/endtotem/events/ModForgeEvents.java
+++ src/main/java/io/github/cuubecon/endtotem/events/ModForgeEvents.java
@@ -11,6 +11,7 @@ import net.minecraft.potion.Effects;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,7 +25,6 @@ public class ModForgeEvents
     @SubscribeEvent
     public static void dimensionChange(PlayerEvent.PlayerChangedDimensionEvent event)
     {
-        System.out.println(event.getPlayer().getPosition());
         BlockPos playerLocation = event.getPlayer().getPosition();
         event.getPlayer().getPersistentData().putIntArray("validPos", new int[]{playerLocation.getX(), playerLocation.getY(), playerLocation.getZ()});
 
@@ -33,12 +33,12 @@ public class ModForgeEvents
     @SubscribeEvent
     public static void onDamage(LivingDamageEvent event)
     {
+
         if(event.getSource().canHarmInCreative() && event.getEntity() instanceof PlayerEntity)
         {
-            System.out.println("onDamage");
-
             ItemStack itemstack = null;
             PlayerEntity player = (PlayerEntity) event.getEntity();
+
             for(Hand hand : Hand.values()) {
                 ItemStack itemstack1 = player.getHeldItem(hand);
                 if (itemstack1.getItem() == ModItems.ENDTOTEM.get()) {
@@ -49,23 +49,17 @@ public class ModForgeEvents
             }
             if(itemstack != null)
             {
-                System.out.println("TOTEM IN HAND");
-
                 if(player.getPersistentData().contains("validPos"))
                 {
                     int[] ints = player.getPersistentData().getIntArray("validPos");
-                    System.out.println(Arrays.toString(ints));
-                    //event.setCanceled(true);
                     player.fallDistance = 0F;
                     player.setPositionAndUpdate(ints[0], ints[1], ints[2]);
                     event.setCanceled(true);
-                    System.out.println("TELEPORT PLAYER");
-
 
                     if (event.getEntity() instanceof ServerPlayerEntity) {
                         ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)event.getEntity();
                         serverplayerentity.takeStat(Stats.ITEM_USED.get(ModItems.ENDTOTEM.get()));
-                        CriteriaTriggers.USED_TOTEM.trigger(serverplayerentity, itemstack);
+                        // CriteriaTriggers.USED_TOTEM.trigger(serverplayerentity, itemstack);
                     }
 
                     player.setHealth(2.0F);
@@ -77,6 +71,43 @@ public class ModForgeEvents
                 }
             }
 
+        }
+        else if(event.getEntity() instanceof PlayerEntity)
+        {
+            PlayerEntity player = (PlayerEntity) event.getEntity();
+            if(player.getHealth() - event.getAmount() <= 0 && player.world.getDimensionKey().getLocation().equals(DimensionType.THE_END_ID))
+            {
+                ItemStack itemstack = null;
+
+                for(Hand hand : Hand.values()) {
+                    ItemStack itemstack1 = player.getHeldItem(hand);
+                    if (itemstack1.getItem() == ModItems.ENDTOTEM.get()) {
+                        itemstack = itemstack1.copy();
+                        itemstack1.shrink(1);
+                        break;
+                    }
+                }
+                if(itemstack != null)
+                {
+                        player.fallDistance = 0F;
+                        event.setCanceled(true);
+
+                        if (event.getEntity() instanceof ServerPlayerEntity) {
+                            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)event.getEntity();
+                            serverplayerentity.takeStat(Stats.ITEM_USED.get(ModItems.ENDTOTEM.get()));
+                            // CriteriaTriggers.USED_TOTEM.trigger(serverplayerentity, itemstack);
+                        }
+
+                        player.setHealth(2.0F);
+                        player.getActivePotionEffects().clear();
+                        player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 900, 1));
+                        player.addPotionEffect(new EffectInstance(Effects.ABSORPTION, 200, 1));
+                    player.addPotionEffect(new EffectInstance(Effects.SATURATION, 50, 1));
+                    player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 800, 0));
+                        player.world.setEntityState(player, (byte)35);
+
+                }
+            }
         }
     }
 }
